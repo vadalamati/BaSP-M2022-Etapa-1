acb3122197240ec52fa1046971c650039d163e9a
Matias <vadalamati@gmail.com>
2022-04-26 13:28:07 -0300

    BaSP-M2022:"some changes for singup"

--- Semana-06/javascript/sign-up.js
+++ Semana-06/javascript/sign-up.js
@@ -1,18 +1,19 @@
 var nombre = document.getElementById('name-sign');
 var apellido = document.getElementById('surName-sign');
-var dni = document.getElementById('dni');
-var fdn = document.getElementById('fdn-sign');
+var doc = document.getElementById('dni');
+var fecNac = document.getElementById('fdn-sign');
 var telefono = document.getElementById('tel-sign');
 var dir = document.getElementById('dir-sign');
-var local = document.getElementById('location-sign');
+var adress = document.getElementById('location-sign');
 var cp = document.getElementById('cp-sign');
 var email = document.getElementById('email-sign');
-var contra = document.getElementById('pass-sign');
-var reContra = document.getElementById('repass-sign');
+var pass = document.getElementById('pass-sign');
+var repass = document.getElementById('repass-sign');
 var hideAlert = document.getElementsByClassName('error-text');
 var validateValue = document.getElementsByClassName('inner-span');
 var validateForm = document.getElementsByClassName('hide-cartel');
 var form = document.getElementsByTagName('form');
+var closeModal = document.getElementById('close-modal');
 
 nombre.onfocus = function () {
     myFocus(nombre, 0);
@@ -26,17 +27,17 @@ apellido.onfocus = function () {
 apellido.onblur = function () {
     myBlur(apellido, 1);
 };
-dni.onfocus = function () {
-    myFocus(dni, 2);
+doc.onfocus = function () {
+    myFocus(doc, 2);
 };
-dni.onblur = function () {
-    myBlur(dni, 2);
+doc.onblur = function () {
+    myBlur(doc, 2);
 };
-fdn.onfocus = function () {
-    myFocus(fdn, 3);
+fecNac.onfocus = function () {
+    myFocus(fecNac, 3);
 };
-fdn.onblur = function () {
-    myBlur(fdn, 3);
+fecNac.onblur = function () {
+    myBlur(fecNac, 3);
 };
 telefono.onfocus = function () {
     myFocus(telefono, 4);
@@ -50,11 +51,11 @@ dir.onfocus = function () {
 dir.onblur = function () {
     myBlur(dir, 5);
 };
-local.onfocus = function () {
-    myFocus(local, 6);
+adress.onfocus = function () {
+    myFocus(adress, 6);
 };
-local.onblur = function () {
-    myBlur(local, 6);
+adress.onblur = function () {
+    myBlur(adress, 6);
 };
 cp.onfocus = function () {
     myFocus(cp, 7);
@@ -68,47 +69,49 @@ email.onfocus = function () {
 email.onblur = function () {
     myBlur(email, 8);
 };
-contra.onfocus = function () {
-    myFocus(contra, 9);
+pass.onfocus = function () {
+    myFocus(pass, 9);
 };
-contra.onblur = function () {
-    myBlur(contra, 9);
+pass.onblur = function () {
+    myBlur(pass, 9);
 };
-reContra.onfocus = function () {
-    myFocus(reContra, 10);
+repass.onfocus = function () {
+    myFocus(repass, 10);
 };
-reContra.onblur = function () {
-    myBlur(reContra, 10);
+repass.onblur = function () {
+    myBlur(repass, 10);
 };
 form[1].onsubmit = function (e) {
     e.preventDefault();
     if (
         validateLetters(nombre, 3) == true &&
         validateLetters(apellido, 3) == true &&
-        validateNumbers(dni, 7) == true &&
+        validateNumbers(doc, 7) == true &&
         validateTel(telefono, 10) == true &&
         validateDir(dir, 5) == true &&
-        validateLocal(local, 3) == true &&
+        validateAdress(adress, 3) == true &&
         validateCp(cp) == true &&
         validateMail(email) == true &&
-        validatePass(contra, 8) == true &&
-        validateRePass(contra, reContra) == true
+        validatePass(pass, 8) == true &&
+        validateRePass(pass, repass) == true
     ) {
+        validateForm[0].classList.remove('hide-cartel');
         validateValue[0].innerHTML = nombre.value;
         validateValue[1].innerHTML = apellido.value;
-        validateValue[2].innerHTML = dni.value;
-        validateValue[3].innerHTML = fdn.value;
+        validateValue[2].innerHTML = doc.value;
+        validateValue[3].innerHTML = fecNac.value;
         validateValue[4].innerHTML = telefono.value;
         validateValue[5].innerHTML = dir.value;
-        validateValue[6].innerHTML = local.value;
+        validateValue[6].innerHTML = adress.value;
         validateValue[7].innerHTML = cp.value;
         validateValue[8].innerHTML = email.value;
-        validateValue[9].innerHTML = contra.value;
-        validateValue[10].innerHTML = reContra.value;
+        validateValue[9].innerHTML = pass.value;
+        validateValue[10].innerHTML = repass.value;
     } else {
-        alert('Algun dato esta mal ingresado');
+        alert('Revise los datos ingresador por favor');
     }
 };
+
 function myFocus(x, i) {
     x.classList.remove('blur');
     hideAlert[i].classList.remove('error');
@@ -272,7 +275,7 @@ function validatePass(x, valor) {
     }
 }
 
-function validateLocal(x, valor) {
+function validateAdress(x, valor) {
     var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     var alph = [
         'a',
